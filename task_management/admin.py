a8a4411ff6174f0e0173c41ed0a0c2a4b097dd58
tranthientam293 <tranthientam2903@gmail.com>
2025-03-31 14:26:30 +0000

    create models

--- task_management/admin.py
+++ task_management/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Role, User, Task, Project, ProjectTask, TaskAssignment, Status, TaskStatus
 
 # Register your models here.
+admin.site.resgiter(Role)
+admin.site.resgiter(User)
+admin.site.resgiter(Task)
+admin.site.resgiter(Project)
+admin.site.resgiter(ProjectTask)
+admin.site.resgiter(TaskAssignment)
+admin.site.resgiter(Status)
+admin.site.resgiter(TaskStatus)
