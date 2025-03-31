31a71c09ed9bf3c8ce3391daec8f531d6644a2c0
tranthientam293 <tranthientam2903@gmail.com>
2025-03-31 14:33:47 +0000

    fix typo

--- task_management/admin.py
+++ task_management/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Role, User, Task, Project, ProjectTask, TaskAssignment, Status, TaskStatus
 
 # Register your models here.
-admin.site.resgiter(Role)
-admin.site.resgiter(User)
-admin.site.resgiter(Task)
-admin.site.resgiter(Project)
-admin.site.resgiter(ProjectTask)
-admin.site.resgiter(TaskAssignment)
-admin.site.resgiter(Status)
-admin.site.resgiter(TaskStatus)
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(Project)
+admin.site.register(ProjectTask)
+admin.site.register(TaskAssignment)
+admin.site.register(Status)
+admin.site.register(TaskStatus)
